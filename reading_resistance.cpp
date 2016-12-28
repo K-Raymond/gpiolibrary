3c565c779a1b64c9b224cdbce746d876f4a4b4a8
Kurtis Raymond <kraymond@sfu.ca>
2016-12-28 01:45:58 -0800

    finnished resistence program

--- reading_resistance.cpp
+++ reading_resistance.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-class Timer
+class Timer // Class to accurately measure the enlapsed time
 {
 public:
     Timer() : beg_(clock_::now()) {}
@@ -32,20 +32,27 @@ int main(void)
     voltage_pin.pin_export(); sense_pin.pin_export();
     voltage_pin.output(); sense_pin.input();
     
-    Timer rise_time(); voltage_pin.set_value("1");
+    double R; // unknown resistence
+    double C = 0.0001; // our known capacitence in farads
     bool voltage_trigger = false;
     string high_or_low;
     double total_time;
+    
+    cout << "Starting timer" << endl;
+    Timer rise_time(); voltage_pin.set_value("1");
+
     while(voltage_trigger)
     {
-        sense_pin.getvalue(high_or_low);
+        sense_pin.getvalue(high_or_low); // dump pin result into var
         if(high_or_low == "1")
         {
-            total_time = rise_time.elapsed();
+            total_time = rise_time.elapsed(); // Total time should be in seconds
             voltage_trigger = true;
         }
     }
     
     // Do math here t=RC, where we know C
-    
+    R = total_time/C;
+    cout << "Our value of resistence is: " << R << endl;
+    return 0;
 }
